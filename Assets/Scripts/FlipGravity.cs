9f7fd368084905044c3c1e43ad5b0e5cab5900ec
Inatigerdream <lougee.ryan@gmail.com>
2023-04-13 08:03:14 -0400

    added mazes and broken gravity

---
+++ Assets/Scripts/FlipGravity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipGravity : MonoBehaviour
+{
+void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.name == "PlayerController")
+    {
+        //invert the PLayerGravity value
+        // collision.GetComponent<
+        //rotate playercontroller z 180 degrees
+        other.transform.Rotate(0, 0, 180);
+        playergravity = other.PlayerGravity.Gravity;
+        playergravity.y = -playergravity.y;
+        
+        // Physics.gravity = new Vector3(0, -9.81f, 0);
+    }
+}
+}
